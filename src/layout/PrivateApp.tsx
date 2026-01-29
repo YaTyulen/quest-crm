8c483bd16f0c9309deddc48279071c2dd88e08be
YaTyulen <yanccch@yandex.ru>
2026-01-29 20:15:32 +0700

    добавление переадресации при перезагрузке и правки мобильного сайдбара

--- src/layout/PrivateApp.tsx
+++ src/layout/PrivateApp.tsx
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
 import { Sidebar } from "../components"
 import AppRouter from "../router/AppRouter"
+import { useNavigate } from "react-router-dom";
+import { BASE_PATH } from "../constants";
 
 
 const PrivateApp = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // срабатывает только при reload
+    if (performance.getEntriesByType('navigation')[0]?.type === 'reload') {
+      navigate(`/${BASE_PATH}/home`, { replace: true });
+    }
+  }, []);
+
+
   return (
     <div className="private-layout">
         {/** Сайдбар и тип того, а в содержательной части - роутер */}
